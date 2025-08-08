766f4d667b5552562844a3cf863936f4e6990922
mengfanzhao <mengfanzhao@bytedance.com>
2025-08-08 18:30:31 +0800

    1 个文件产生了变更，影响了1 个 函数
    更新函数「testFunction」

--- functions/nodejs_20/testFunction/index.js
+++ functions/nodejs_20/testFunction/index.js
@@ -9,10 +9,9 @@
  *
  * @return 函数的返回数据
  */
-module.exports = async function (params, context, logger) {
+module.exports = async function (ru, chu, logger) {
   // 日志功能
   // logger.info(`${new Date()} 函数开始执行`);
-  logger.info(params)
-  logger.info(context)
+  logger.info(ru)
   // 在这里补充业务代码
 }
